49b0c0eb436aab8388b269f45d187cb18461324f
rayhanramin <55429573+rayhanramin@users.noreply.github.com>
2020-03-31 01:49:28 -0500

    Update tests_forms.py

--- mysite/main/tests/tests_forms.py
+++ mysite/main/tests/tests_forms.py
@@ -1,31 +1,25 @@
-
-
-
-
 from django.test import TestCase
 from main.forms import NewUserForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm	
+from .models import Tweet	
+from .forms import TweetForm	
+from django.urls import reverse	
 
+# Create your tests here.	
 
+class TweetFormTest(TestCase):	
 
+    def test_tweet_form(self):	
+        form = TweetForm({	
+            'tweet_content': "This is my first Tweet",	
+            }, )	
+        self.assertTrue(form.is_valid())	
+        tweet = form.save()	
+        self.assertEqual(tweet.tweet_content, 'This is my first Tweet')	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class TestForms(TestCase):
+    def test_empty_tweet_form(self):	
+        form = TweetForm({},)	
+        self.assertFalse(form.is_valid())
 
 	def test_new_user_form_valid_data(self):
 		form = NewUserForm(data={
@@ -42,8 +36,6 @@ class TestForms(TestCase):
 		form = NewUserForm(data={})
 		self.assertFalse(form.is_valid())
 		self.assertEquals(len(form.errors),6)
-
-
 
 	def test_authenticate_forms_no_data(self):
 		n_form = AuthenticationForm(data={})
