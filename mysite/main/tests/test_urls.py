ab2fc65e71952bf283831e7b11d406288aaa99ee
MD RAYHAN AMIN <55429573+rayhanramin@users.noreply.github.com>
2020-04-13 21:59:49 -0500

    Fixed a few issues

--- mysite/main/tests/test_urls.py
+++ mysite/main/tests/test_urls.py
@@ -1,15 +1,11 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve, path
-from main.views import homepage, register, login_request, logout_request
+from main.views import account, register, login_request, logout_request
 import json
 
 # Create your tests here.
 
 class TestUrls(SimpleTestCase):
-
-	def test_homepage_url_is_resolved(self):
-		url = reverse('main:homepage')
-		self.assertEquals(resolve(url).func,homepage)
 		
 	def test_login_url_is_resolved(self):
 		url = reverse('main:login')
@@ -22,7 +18,9 @@ class TestUrls(SimpleTestCase):
 	def test_register_url_is_resolved(self):
 		url = reverse('main:register')
 		self.assertEquals(resolve(url).func,register)
-
-
+		
+	def test_account_url_is_resolved(self):
+		url = reverse('main:account')
+		self.assertEquals(resolve(url).func,account)
 
 		
