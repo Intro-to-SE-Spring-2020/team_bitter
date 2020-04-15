b72067555416fcf7914165ef6dac3d6a4b00ab4e
MD RAYHAN AMIN <55429573+rayhanramin@users.noreply.github.com>
2020-04-14 22:57:19 -0500

    Added new test cases in the tests_forms.py file

--- mysite/main/tests/tests_forms.py
+++ mysite/main/tests/tests_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from main.forms import NewUserForm
+from main.forms import NewUserForm, AddFriendForm, DeleteFriendForm, BlockFriendForm, UnBlockFriendForm
 from django.contrib.auth.forms import AuthenticationForm	
 from main.models import Tweet	
 from main.forms import TweetForm	
@@ -30,4 +30,36 @@ class TweetFormTest(TestCase):
 		self.assertFalse(n_form.is_valid())
 		self.assertEquals(len(n_form.errors),2)
 
-
+class AddFriendFormTest(TestCase):
+	def test_AddFriendForm_valid_data(self):
+		form = AddFriendForm(data={'friendWaitToAdd': 'AddFriendForm'}) 
+		self.assertTrue(form.is_valid())
+	def test_AddFriendForm_no_data(self):
+		form = AddFriendForm(data={})
+		self.assertFalse(form.is_valid())
+		
+class DeleteFriendFormTest(TestCase):
+	def test_DeleteFriendForm_valid_data(self):
+		form = DeleteFriendForm(data={'friendWaitToDelete': 'DeleteFriendForm'}) 
+		self.assertTrue(form.is_valid())
+	def test_DeleteFriendForm_no_data(self):
+		form = DeleteFriendForm(data={})
+		self.assertFalse(form.is_valid())
+		
+class BlockFriendFormTest(TestCase):
+	def test_BlockFriendForm_valid_data(self):
+		form =BlockFriendForm(data={'friendWaitToDelete': 'BlockFriendForm'}) 
+		self.assertFalse(form.is_valid())
+	def test_BlockFriendForm_no_data(self):
+		form = BlockFriendForm(data={})
+		self.assertFalse(form.is_valid())
+		
+class UnBlockFriendFormTest(TestCase):
+	def test_UnBlockFriendForm_valid_data(self):
+		form =UnBlockFriendForm(data={'friendWaitToUnblock': 'BlockFriendForm'}) 
+		self.assertFalse(form.is_valid())
+	def test_UnBlockFriendForm_no_data(self):
+		form = UnBlockFriendForm(data={})
+		self.assertFalse(form.is_valid())
+		
+		
